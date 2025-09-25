f3bb0c7a28fc7fb24994c96735f5ad110bc377cf
ivenzor <ivenzor@gmail.com>
2025-09-25 00:52:03 -0500

    exclude stellar variability points with mag_err >= 1

--- exotic/output_files.py
+++ exotic/output_files.py
@@ -168,9 +168,10 @@ class AIDOutputFiles:
 
             f.write("#NAME,DATE,MAG,MERR,FILT,TRANS,MTYPE,CNAME,CMAG,KNAME,KMAG,AMASS,GROUP,CHART,NOTES\n")
             for vsp_p in self.vsp_params:
-                f.write(f"{self.auid},{round(vsp_p['time'], 5)},{round(vsp_p['mag'], 5)},{round(vsp_p['mag_err'], 4)},"
-                        f"{self.i_dict['filter']},NO,STD,{vsp_p['cname']},{round(vsp_p['cmag'], 5)},na,na," 
-                        f"{round(vsp_p['airmass'], 7)},na,{self.chart_id},na\n")
+                if vsp_p['mag_err'] < 1:  # exclude stellar variability points with mag_err >= 1
+                    f.write(f"{self.auid},{round(vsp_p['time'], 5)},{round(vsp_p['mag'], 5)},{round(vsp_p['mag_err'], 4)},"
+                            f"{self.i_dict['filter']},NO,STD,{vsp_p['cname']},{round(vsp_p['cmag'], 5)},na,na," 
+                            f"{round(vsp_p['airmass'], 7)},na,{self.chart_id},na\n")
 
 
 def aavso_dicts(planet_dict, fit, info_dict, durs, ld0, ld1, ld2, ld3):
